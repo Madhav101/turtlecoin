841a24539ad573b4e6de716427e9a620f9eb122e
Madhav101 <46110975+Madhav101@users.noreply.github.com>
2019-04-30 02:54:08 +0530

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -10,7 +10,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = "dm";
+    const std::string_view addressPrefix = "dmz";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "DM";
